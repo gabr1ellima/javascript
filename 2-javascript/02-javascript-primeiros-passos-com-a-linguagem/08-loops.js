8e2cd1a96f04b1578eee1a4f07bbd27fa7cfad24
Gabriel Lima <gabriellima0409@gmail.com>
2021-05-13 13:58:44 -0300

    Iterando sobre coleções

---
+++ 2-javascript/02-javascript-primeiros-passos-com-a-linguagem/08-loops.js
@@ -0,0 +1,42 @@
+console.log(`Trabalhando com condicionais`);
+
+const listaDeDestinos = new Array(
+  `Salvador`,
+  `São Paulo`,
+  `Rio de Janeiro`
+);
+
+const idadeComprador = 17;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Salvador";
+
+console.log("\nDestinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+let destinoExiste = false;
+
+while(contador < listaDeDestinos.length){
+  if(listaDeDestinos[contador] == destino) {
+    destinoExiste = true;
+    break;
+  }
+  contador++;
+}
+
+console.log("Destino existe:", destinoExiste);
+
+if(podeComprar && destinoExiste) {
+  console.log("Boa viagem");
+} else {
+  console.log("Desculpe, ocorreu um erro!");
+}
+
+for(let i = 0 ; i < listaDeDestinos.length ; i++){
+  if(listaDeDestinos[contador] == destino) {
+    destinoExiste = true;
+  }
+}
