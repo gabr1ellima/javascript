9b7cdfa34ed4f65647c1eb101c1cc8d0f2296cfb
Gabriel Lima <gabriellima0409@gmail.com>
2021-05-13 07:54:59 -0300

    Tipos de variáveis

---
+++ 2-javascript/02-javascript-primeiros-passos-com-a-linguagem/05-atribuicao-de-variaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 22;
+let primeiroNome = "Gabriel";
+const sobrenome = "Lima";
+
+// console.log(primeiroNome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu primeiroNome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + " " + sobrenome;
+
+console.log(nomeCompleto);
