f2afacd39dde1fc94688cfc3eee4e4489cdb94cf
Gabriel Lima <gabriellima0409@gmail.com>
2021-05-27 16:37:29 -0300

    Aula 04 - Sistema interno

--- 2-javascript/03-javascript-orientacao-a-objetos/index.js
+++ 2-javascript/03-javascript-orientacao-a-objetos/index.js
@@ -1,15 +1,20 @@
-import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { Cliente } from './Cliente.js'
+import { Gerente } from './Funcionario/Gerente.js'
+import { Diretor } from './Funcionario/Diretor.js'
+import { SistemaAutenticacao } from './SistemaAutenticacao.js';
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
+const diretor = new Diretor('Rodrigo', 10000, 12345678900);
+const gerente = new Gerente('Ricardo', 5000, 12378945601);
+const cliente = new Cliente('Lais', 789542344112, '456');
 
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+diretor.cadastrarSenha('123456')
+gerente.cadastrarSenha('123');
 
-console.log(contaSalario);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456');
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123');
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, '4565');
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
+
+
